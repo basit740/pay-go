d09bc9cd0235ff2cb4a2d021c7ceda9267b22e61
Basit Ali <alib4111@gmail.com>
2023-07-22 05:15:16 +0500

    optimized modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Modal.css';
 const Index = ({ children, onClose, open }) => {
+	if (open) {
+		document.querySelector('body').style.overflow = 'hidden';
+	}
 	return (
 		<div className={`modal ${open ? 'open' : ''}`} onClick={onClose}>
 			<div className='modal__content'>{children}</div>
